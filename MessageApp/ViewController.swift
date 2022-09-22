03d07f5ee002c1888315545c571b4d0f101bab23
Polina <112948671+apolinarys@users.noreply.github.com>
2022-09-22 17:28:44 +0300

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Macbook on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
