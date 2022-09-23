4029a00893bb38152386b52b730c38981466d381
Polina <112948671+apolinarys@users.noreply.github.com>
2022-09-23 15:15:31 +0300

    Add application icons

--- MessageApp/ViewController.swift
+++ MessageApp/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let logger = Logger()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logger.logging(funcName: "viewDidLoad")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        logger.logging(funcName: "viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logger.logging(funcName: "viewDidAppear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        logger.logging(funcName: "viewWillLayoutSubviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        logger.logging(funcName: "viewDidLayoutSubviews")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        logger.logging(funcName: "viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        logger.logging(funcName: "viewDidDisappear")
+    }
 
 }
 
